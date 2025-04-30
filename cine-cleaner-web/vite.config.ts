7d4f7e36c3f4549013b9172749bdf156c5a14557
Joseph Gilgen <jgilgen@theabr.org>
2025-04-30 08:34:25 -0700

    fix base path

--- cine-cleaner-web/vite.config.ts
+++ cine-cleaner-web/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite"
-import solid from "vite-plugin-solid"
-import { resolve } from "node:path"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+import { resolve } from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
   },
   plugins: [solid(), tailwindcss()],
-  optimizeDeps: { exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/utils"] },
-})
+  optimizeDeps: { exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/utils'] },
+  base: '/cine-cleaner/',
+});
